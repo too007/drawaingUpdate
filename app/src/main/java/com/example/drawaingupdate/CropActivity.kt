0f07fd19d9cc7ea0a7ebac04a2809f029a022027
vir-webdev <viratgamit1.@gmail.com>
2024-08-04 22:21:47 +0530

    sagar

---
+++ app/src/main/java/com/example/drawaingupdate/CropActivity.kt
@@ -0,0 +1,17 @@
+package com.example.drawaingupdate
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CropActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_crop)
+
+    }
+}
+git remote set-url origin https://github.com/too007/drawaingUpdate.git
---
+++ app/src/main/java/com/example/drawaingupdate/ImageAdapter.kt
@@ -0,0 +1,32 @@
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.drawaingupdate.R
+
+class ImageAdapter(private val imageUris: List<Uri>, private val context: Context) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val uri = imageUris[position]
+        holder.imageView.setImageURI(uri)
+    }
+
+    override fun getItemCount(): Int {
+        Log.e("TAG", "getItemCount: "+imageUris.size )
+        return imageUris.size
+    }
+}
